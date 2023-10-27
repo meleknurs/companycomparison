622d890b54b00bf9ba8b9f3f6a1c96e9bd4b3333
faranak <faranak.rahimi@powercoders.org>
2023-10-27 15:27:51 +0200

    add function to filter it and show us the compony

---
+++ script.js
@@ -0,0 +1,20 @@
+import myJson from './JSON.json' assert {type: 'json'};
+//
+ let filterByCom =myJson.filter(FltrFun)
+ 
+ filterByCom.forEach(displayCom)
+ function displayCom (data) {
+    
+    const ulTag= document.getElementById("demo");
+    const ilTag = document.createElement("li");
+    ilTag.innerHTML=(data.company);
+    ulTag.appendChild(ilTag);
+ }
+
+ function FltrFun(data) {
+    if(data.location.includes("Bern"))
+    return true;
+ }
+
+    
+ 
